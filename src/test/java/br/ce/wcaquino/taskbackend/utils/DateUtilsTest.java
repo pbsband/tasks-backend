1130dd6427ec69b7002bae57fc12f33e1167f873
unknown <pbsband@yahoo.com.br>
2020-05-06 19:46:41 -0300

    first commit

--- src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -6,23 +6,22 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class DateUtilsTest {
-    
+
 	@Test
-	public void deveRetornarTrueParaDatasFuturas () {
+	public void deveRetornarTrueParaDatasFuturas() {
 		LocalDate date = LocalDate.of(2030, 01, 01);
 		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
 	}
 	
 	@Test
-	public void deveRetornarFalseParaDatasPassadas () {
+	public void deveRetornarFalseParaDatasPassadas() {
 		LocalDate date = LocalDate.of(2010, 01, 01);
 		Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
 	}
-
+	
 	@Test
-	public void deveRetornarTrueParaDataAtual () {
+	public void deveRetornarTrueParaDataAtual() {
 		LocalDate date = LocalDate.now();
 		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
 	}
-	
 }
